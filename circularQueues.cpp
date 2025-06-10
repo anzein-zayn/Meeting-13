f47215eb67b1aa598a33e672f066316e3e9eb19f
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-06-10 11:11:55 +0700

    membuat else, jika font posisi lebih besar dari posisi near, iterasi front hingga akhir array

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -78,4 +78,19 @@ void display(){
         }
         cout<<endl;
     }
+    else{
+        while(front_position <= max - 1) {
+            cout << queue_array[front_position] << "  ";
+            front_position++;
+        }
+
+        front_position = 0;
+
+        while(front_position <=rear_position){
+            cout << queue_array[front_position] << "  ";
+            front_position++;
+        }
+        cout<<endl;
+    }
 }
+};
